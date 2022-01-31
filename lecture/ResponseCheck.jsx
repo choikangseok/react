533af3c52599497391957afacd15b3a124d06d12
choikangseok <ktos5427@naver.com>
2022-01-31 12:48:10 +0900

    performance up

--- lecture/ResponseCheck.jsx
+++ lecture/ResponseCheck.jsx
@@ -2,21 +2,63 @@ import React, {Component} from 'react';
 
 
 class ResponseCheck extends Component {
+    
     state ={
         state : 'waiting',
         message: '클릭해서 시작하세요',
         result: []
     }; 
+    timeout;
 
     onClickScreen = () => {
+        const {state, message, result} = this.state;
+        if (state ==="waiting"){
+            this.setState( {
+                state: 'ready',
+                message: '초록색이 되면 클릭하세요.',
+            });
+            this.timeout = setTimeout( () => {
+                this.setState({
+                    state: 'now',
+                    message : "지금 클릭",
+                })
+                this.startTime = new Date();
+            }, Math.floor(Math.random() * 1000) + 2000);
+        } else if (state === 'ready') {
+            clearTimeout(this.timeout)
+            this.setState({
+                state: "waiting",
+                message : "성급하시네요, 초록색이 된 후에 클릭하세요."
+            })
+            
 
+        } else if (state === "now") {//반응속도 체크
+            this.endTime = new Date();
+            
+            this.setState((prevState) =>{
+                return {
+                    state: "waiting",
+                    message : "클릭해서 시작하세요.",
+                    result: [...prevState.result, this.endTime - this.startTime],
+                };
+            });
+        }
+    };
+
+    onReset = () =>{
+        this.setState({
+            result : [],
+        });
     };
 
     renderAverage = () =>{
         const {result} = this.state;
-        return result.length ===0
+        return result.length === 0
         ? null
-        : <div>평균 시작 : {this.state,result.reduce((a, c) => a+ c) / this.state.result.length}ms</div>
+        : <>
+            <div>평균 시작 : {this.state,result.reduce((a, c) => a+ c) / this.state.result.length}ms</div>
+            <button onClick = {this.onReset}>리셋</button>
+        </>
     };
 
     render(){
